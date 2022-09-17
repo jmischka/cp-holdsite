5490a05b9cc16eb29042fd198caae878da8609ec
Jason Mischka <jasonmischka@Jasons-MBP.fios-router.home>
2022-09-17 18:10:23 -0400

    updated email address

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <h2>New Website<br />Coming Soon</h2>
         <p>For more information about our services or to speak with a member of our team please contact us at:</p>
         <p>800.222.3456</p>
-        <a href='mailto: jasonmischka@mac.com'>info@cruiseportdestinations.com</a>
+        <a href='mailto: info@cruiseportdestinations.com'>info@cruiseportdestinations.com</a>
       </div>
       <div className='Boat-art'>
         <img src={boat} alt="animated graphic" />
