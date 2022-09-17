b6f59a2f217b2d1fe0d57bacc2ec6d5bf5959ae7
Jason Mischka <jasonmischka@Jasons-MBP.fios-router.home>
2022-09-17 17:53:50 -0400

    added mobile breakpoints

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} />
+        <img src={logo} alt="Cruiseport Logo" />
       </header>
       <div className='App-body'>
         <h2>New Website<br />Coming Soon</h2>
-        <p>For more information about our services or to speak with a member<br />of our team please contact us at:</p>
+        <p>For more information about our services or to speak with a member of our team please contact us at:</p>
         <p>800.222.3456</p>
         <a href='mailto: jasonmischka@mac.com'>info@cruiseportdestinations.com</a>
       </div>
       <div className='Boat-art'>
-        <img src={boat} />
+        <img src={boat} alt="animated graphic" />
       </div>
     </div>
   );
